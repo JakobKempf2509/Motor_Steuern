0013f09d3aa8e4ca4c6458c614e8015016d2ae3a
Kempf <jakob.kempf@meiko-global.com>
2020-04-22 14:44:19 +0200

    FB_Steuerung_des_Motors_mit_Freigabe

--- Datenverarbeitung.py
+++ Datenverarbeitung.py
@@ -1,3 +1,5 @@
+#!/usr/bin/env python
+
 #Dieses Skript dient zum Weiterverarbeitung der Daten
 #J. Kempf 2020-04-22
 
@@ -7,45 +9,57 @@ import RPi.GPIO as GPIO
 import time
 from mfrc522 import SimpleMFRC522
 
+#benötigte Festlegungen
+abbruch = True
+Benutzer_1 = 733256096369
+
 #Zuweisung der Erstellfunktion zu einer Variable (Assignment of the creation variable to a variable)
-my_reader = SimpleMFRC522
+my_reader = SimpleMFRC522()
 
-#Leseanweisung und Ausgabe der eingelesenen Daten (Reading instructions and output of the read data)
-try:
-    id, my_text = my_reader.read()
-
-    if (id == 733256096369 ):
-        #Festlegung des Pins für die Steuerung
-        servoPIN = 22
-        GPIO.setmode(GPIO.BCM)
-        GPIO.setup(servoPIN, GPIO.OUT)
-
-        p = GPIO.PWM(servoPIN, 50) # GPIO 22 als PWM mit 50Hz
-        p.start(2.5) # Initialisierung
-        p.ChangeDutyCycle(5)
-        time.sleep(0.5)
-        p.ChangeDutyCycle(7.5)
-        time.sleep(0.5)
-        p.ChangeDutyCycle(10)
-        time.sleep(0.5)
-        p.ChangeDutyCycle(12.5)
-        time.sleep(0.5)
-        p.ChangeDutyCycle(10)
-        time.sleep(0.5)
-        p.ChangeDutyCycle(7.5)
-        time.sleep(0.5)
-        p.ChangeDutyCycle(5)
-        time.sleep(0.5)
-        p.ChangeDutyCycle(2.5)
-        time.sleep(0.5)
-
-        #Prozess stoppen und alle GPIO auf Input stellen
-        p.stop()  
+#Schleife zur Steuerung des Motors mit Freigabe (loop to control the engine with release)
+while abbruch:
+    try:
+        #Zuweisung der ID und des Inhalts des Tags
+        print("Bitte autorisieren Sie sich als Benutzer indem Sie ihren RFID-Tag auf den Leser legen")
+        id, my_text = my_reader.read()
         GPIO.cleanup()
 
-    else:
-        print("Der Benutzer ist nicht autorisiert")
-        Abbruch = True
+        #If-Bedingung für richtige ID
+        if (id == Benutzer_1):
+            print("Der Benutzer ist für die gewünschte Anforderung autorisiert")
 
-except ValueError:
-    print("Error")
+            #Festlegung des Pins für die Steuerung
+            servoPIN = 22
+            GPIO.setmode(GPIO.BCM)
+            GPIO.setup(servoPIN, GPIO.OUT)
+
+            p = GPIO.PWM(servoPIN, 50) # GPIO 22 als PWM mit 50Hz
+            p.start(2.5) # Initialisierung
+            p.ChangeDutyCycle(5)
+            time.sleep(0.5)
+            p.ChangeDutyCycle(7.5)
+            time.sleep(0.5)
+            p.ChangeDutyCycle(10)
+            time.sleep(0.5)
+            p.ChangeDutyCycle(12.5)
+            time.sleep(0.5)
+            p.ChangeDutyCycle(10)
+            time.sleep(0.5)
+            p.ChangeDutyCycle(7.5)
+            time.sleep(0.5)
+            p.ChangeDutyCycle(5)
+            time.sleep(0.5)
+            p.ChangeDutyCycle(2.5)
+            time.sleep(0.5)
+
+            #Prozess stoppen und alle GPIO auf Input stellen
+            p.stop()  
+            print("Die gewüschte Anwendung wurde erfolgreich durchgeführt.\nDie Anwendung wird im Anschluss beendet.")
+            GPIO.cleanup()
+            abbruch = False
+
+        else:
+            print("Sie sind als Benutzer nicht für diese Anwendung autorisiert, bitte versuchen Sie es nochmal")
+
+    except ValueError:
+        print("Es gab einen Fehler bei dem gewünschten Prozess, bitte versuchen Sie es erneut")
